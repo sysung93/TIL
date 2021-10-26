23f36157a63438620cf61d7ff7bade0377263714
sysung93 <sysung93@gmail.com>
2021-10-26 21:00:59 +0900

    getter and setter

---
+++ Java/JavaStudy/src/chapter6/getter_setter/Car.java
@@ -0,0 +1,26 @@
+package chapter6.getter_setter;
+
+public class Car {
+    private int speed;
+    private boolean stop;
+
+    public int getSpeed() {
+        return  speed;
+    }
+
+    public boolean isStop() {
+        return stop;
+    }
+
+    public void setSpeed(int speed) {
+        if(speed < 0) {
+            this.speed = 0;
+        } else {
+            this.speed = speed;
+        }
+    }
+
+    public void setStop(boolean stop) {
+        this.stop = stop;
+    }
+}
---
+++ Java/JavaStudy/src/chapter6/getter_setter/CarExample.java
@@ -0,0 +1,13 @@
+package chapter6.getter_setter;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.setSpeed(-60);
+        myCar.setStop(false);
+
+        System.out.println(myCar.getSpeed());
+        System.out.println(myCar.isStop());
+    }
+}
