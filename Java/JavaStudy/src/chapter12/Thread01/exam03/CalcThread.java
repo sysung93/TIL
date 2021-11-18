11e6879d8703b07c52ce34bf1f81db32b7ec9df4
sysung93 <sysung93@gmail.com>
2021-11-18 23:03:06 +0900

    .스레드우선순위

---
+++ Java/JavaStudy/src/chapter12/Thread01/exam03/CalcThread.java
@@ -0,0 +1,14 @@
+package chapter12.Thread01.exam03;
+
+public class CalcThread extends Thread{
+    public CalcThread(String name) {
+        setName(name);
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < 2000000000; i++) {
+        }
+        System.out.println(getName());
+    }
+}
