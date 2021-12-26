07acc5bf91a4e9c9e65e70debd994f86c93c6bca
sysung93 <sysung93@gmail.com>
2021-12-26 22:51:00 +0900

    CSS3 기본

--- Java/multicampus_workspace/algorismPropject1/algorismProject2/src/algorismProject2/BinTreeTester.java
+++ Java/multicampus_workspace/algorismPropject1/algorismProject2/src/algorismProject2/BinTreeTester.java
@@ -1,16 +1,17 @@
+package algorismProject2;
 import java.util.Scanner;
-// 이진검색트리 클래스 BinTree<K,V>의 이용 예
+
 
 class BinTreeTester {
 	static Scanner stdIn = new Scanner(System.in);
 
 	// 데이터 (회원번호 + 이름)
 	static class Data {
-		public static final int NO   = 1;	// 번호를 읽어 들일까요?
-		public static final int NAME = 2;	// 이름을 읽어 들일까요?
+		public static final int NO   = 1;	
+		public static final int NAME = 2;	
 
-		private Integer no;					// 회원번호 (키 값)
-		private String  name;				// 이름
+		private Integer no;					
+		private String  name;				
 
 		// 키 값
 		Integer keyCode() {
@@ -44,20 +45,20 @@ class BinTreeTester {
 		PRINT(    "전체 상품 조회"),
 		TERMINATE("종료");
 
-		private final String message;			// 출력할 문자열 
+		private final String message;			 
 
-		static Menu MenuAt(int idx) {			// 서수가 idx인 열거를 반환
+		static Menu MenuAt(int idx) {			
 			for (Menu m : Menu.values())
 				if (m.ordinal() == idx)
 					return m;
 			return null;
 		}
 
-		Menu(String string) {					// 생성자
+		Menu(String string) {					
 			message = string;
 		}
 
-		String getMessage() {					// 출력할 문자열을 반환
+		String getMessage() {					
 			return message;
 		}
 	}
@@ -76,29 +77,29 @@ class BinTreeTester {
 	}
 
 	public static void main(String[] args) {
-		Menu menu;								// 메뉴
-		Data data = null;								// 추가용 데이터 참조
-		Data ptr;								// 검색용 데이터 참조
-		Data temp = new Data();					// 입력용 데이터
+		Menu menu;								
+		Data data = null;						
+		Data ptr;								
+		Data temp = new Data();					
 		BinTree<Integer, Data> tree = new BinTree<Integer, Data>();
 		
 		
 		
 		do {
 			switch (menu = SelectMenu()) {
-			 case ADD :							// 노드를 삽입
+			 case ADD :							
 					data = new Data();
 			 		data.scanData("상품 등록", Data.NO | Data.NAME);
 			 		tree.add(data.keyCode(), data);
 			 		break;
 
-			 case REMOVE :						// 노드를 삭제
+			 case REMOVE :						
 					temp.scanData("상품 삭제", Data.NO);
 			 		tree.remove(temp.keyCode());
 			 		System.out.println("상품 삭제 완료");
 			 		break;
 
-			 case SEARCH :						// 노드를 검색
+			 case SEARCH :						
 					temp.scanData("상품 검색", Data.NO);
 			 		ptr = tree.search(temp.keyCode());
 			 		if (ptr != null)
@@ -106,14 +107,13 @@ class BinTreeTester {
 					else
 			 			System.out.println("등록된 상품이 없습니다.");
 			 		break;
-			 case PRINT :						// 모든 노드를 키 값의 오름차순으로 출력				 	
+			 case PRINT :										 	
 				 	if (data != null)
 						tree.print();
 					else
 				 		System.out.println("등록된 상품이 없습니다.");
 				 	break;
-//				 	tree.print();
-//					break;
+
 			}
 		} while (menu != Menu.TERMINATE);
 		
