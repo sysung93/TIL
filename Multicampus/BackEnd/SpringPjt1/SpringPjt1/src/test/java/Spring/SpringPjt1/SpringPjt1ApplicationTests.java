e4968e6afd8fb7eea3b054be61b14d7328679006
sysung93 <sysung93@gmail.com>
2022-01-24 23:45:10 +0900

    spring

---
+++ Multicampus/BackEnd/SpringPjt1/SpringPjt1/src/test/java/Spring/SpringPjt1/SpringPjt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.SpringPjt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPjt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
