b24fe33cdc3475ea054b2945389fea9b5e9b6272
sysung93 <sysung93@gmail.com>
2022-02-01 17:28:31 +0900

    Spring 비동기처리

--- Back_End/Spring/Spring/Theory/async/src/main/java/com/example/async/controller/ApiController.java
+++ Back_End/Spring/Spring/Theory/async/src/main/java/com/example/async/controller/ApiController.java
@@ -1,4 +1,24 @@
 package com.example.async.controller;
 
+import com.example.async.service.AsyncService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api")
 public class ApiController {
+    private final AsyncService asyncService;
+
+    @GetMapping("hello")
+    public CompletableFuture hello(){
+        log.info("completable future init");
+        return asyncService.run();
+    }
 }
