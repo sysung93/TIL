3357c1618189b4c9e61924f2c1063a1636741355
sysung93 <sysung93@gmail.com>
2022-02-02 14:49:30 +0900

    Spring JUnit으로 테스트하기

---
+++ Back_End/Spring/Spring/JunitTest/calculator/src/main/java/MarketApi.java
@@ -0,0 +1,7 @@
+public class MarketApi {
+    public int connect() {
+        //naver
+        //Kakao
+        return 1100;
+    }
+}
---
+++ Back_End/Spring/Spring/JunitTest/spring-calculator/src/main/java/com/example/springcalculator/component/MarketApi.java
@@ -0,0 +1,12 @@
+package com.example.springcalculator.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+    public int connect() {
+        //naver
+        //Kakao
+        return 1100;
+    }
+}
