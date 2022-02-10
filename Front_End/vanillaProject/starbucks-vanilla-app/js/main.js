4be821cea030a14de2c4eb375ba63da0e97b609b
sysung93 <sysung93@gmail.com>
2022-02-10 22:43:17 +0900

    starbuks-vanilla-app(3)

--- Front_End/vanillaProject/starbucks-vanilla-app/js/main.js
+++ Front_End/vanillaProject/starbucks-vanilla-app/js/main.js
@@ -1,22 +1,3 @@
-// html의 search > input을 찾는다
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input')
-
-// search에 클릭 이벤트 추가
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add('focused');
-  //searchInputEl에 html을 추가하겠다
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur',function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
 
 //뱃지 사라지게하기
 const toTopEl = document.querySelector('#to-top');
@@ -156,6 +137,5 @@ spyEls.forEach(function(spyEl) {
 });
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
+
 
