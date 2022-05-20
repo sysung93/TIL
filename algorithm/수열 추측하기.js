e3bd09d4da53e69adce55a53626b5e70373a4891
sysung93 <sysung93@gmail.com>
2022-05-20 17:46:29 +0900

    수열추측하기(순열,이항계수 응용)

---
+++ algorithm/수열 추측하기.js
@@ -0,0 +1,46 @@
+function solution(n, f) {
+  let answer,
+    flag = 0;
+
+  // 11 * 11 2차원배열만들어서 값 저장할 배열 만들기
+  let dy = Array.from(Array(11), () => Array(11).fill(0));
+  // 순열 check 배열
+  let ch = Array.from({ length: n + 1 }, () => 0);
+  // 조합 배열
+  let p = Array.from({ length: n }, () => 0);
+  // combi 저장할 배열
+  let b = Array.from({ length: n }, () => 0);
+
+  // combi 함수 n-1Cr-1 + n-1Cr
+  function combi(n, r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (n === r || r === 0) return 1;
+    else return (dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r));
+  }
+
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n && sum === f) {
+      answer = p.slice();
+      flag = 1;
+    } else {
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          p[L] = i;
+          DFS(L + 1, sum + b[L] * p[L]);
+          ch[i] = 0;
+        }
+      }
+    }
+  }
+  // combi 저장 4개원소 3C0, 3C1, 3C2, 3C3
+  for (let i = 0; i < n; i++) {
+    b[i] = combi(n - 1, i);
+  }
+
+  DFS(0, 0);
+  return answer;
+}
+
+console.log(solution(4, 16));
