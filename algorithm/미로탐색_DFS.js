383b93502bd3eed5bc9c993d3134f9ee414b183f
sysung93 <sysung93@gmail.com>
2022-05-25 22:36:35 +0900

    미로탐색(DFS)

---
+++ algorithm/미로탐색_DFS.js
@@ -0,0 +1,35 @@
+function solution(arr) {
+  let answer = 0;
+  let dy = [-1, 0, 1, 0];
+  let dx = [0, 1, 0, -1];
+
+  function DFS(x, y) {
+    if (x === 6 && y === 6) {
+      answer++;
+    } else {
+      for (let k = 0; k < 4; k++) {
+        let nx = x + dx[k];
+        let ny = y + dy[k];
+        if (nx >= 0 && nx <= 6 && ny >= 0 && ny <= 6 && arr[nx][ny] === 0) {
+          arr[nx][ny] = 1;
+          DFS(nx, ny);
+          arr[nx][ny] = 0;
+        }
+      }
+    }
+  }
+
+  arr[0][0] = 1;
+  DFS(0, 0);
+  return answer;
+}
+let arr = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 0, 0, 0, 0, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0],
+];
+console.log(solution(arr));
