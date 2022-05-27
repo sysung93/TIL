691539266e87de3145d4d27d6b20128c3f027fb2
sysung93 <sysung93@gmail.com>
2022-05-27 22:27:29 +0900

    섬나라아일랜드(DFS)

---
+++ algorithm/섬나라아일랜드_DFS활용.js
@@ -0,0 +1,42 @@
+function solution(board) {
+  let answer = 0;
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+  function DFS(x, y) {
+    // 지나온길은 0으로 체크
+    board[x][y] = 0;
+    // 8방향 탐색
+    for (let k = 0; k < 8; k++) {
+      let nx = x + dx[k];
+      let ny = y + dy[k];
+      if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+        DFS(nx, ny);
+      }
+    }
+  }
+
+  // 보드 탐색하기
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      // 보드를 탐색했을 때 길이면 answer에 누적, DFS로 넘기기
+      if (board[i][j] === 1) {
+        answer++;
+        DFS(i, j);
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+];
+console.log(solution(arr));
