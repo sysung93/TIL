1458c2859775f7bea2ccef852bb1b85a0c339516
sysung93 <sysung93@gmail.com>
2022-06-08 14:20:53 +0900

    bj 8393, 2741, 2742, 11021

---
+++ algorithm/baekjoon/반복문/8393/app.js
@@ -0,0 +1,15 @@
+// fs모듈을 사용해서 입력을 읽음
+const fs = require("fs");
+// 백준은 linux환경이기때문에 경로를 다르게 함
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("/n");
+
+solution(+input[0]);
+
+function solution(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  console.log(sum);
+}
