780ab6296600ff9e9a5fbf7d25dc6be17bdbb672
sysung93 <sysung93@gmail.com>
2022-06-10 15:41:36 +0900

    bj 10952, 10951, 1110

---
+++ algorithm/baekjoon/반복문/1110/app.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+solution(+input[0]);
+
+function solution(N) {
+  let makeNum = N;
+  let cnt = 0;
+
+  while (makeNum !== N || cnt === 0) {
+    let ten;
+    let one;
+
+    if (makeNum >= 10) {
+      ten = +String(makeNum)[0];
+      one = +String(makeNum)[1];
+    } else {
+      ten = 0;
+      one = +String(makeNum)[0];
+    }
+
+    makeNum = ten + one;
+    
+    let makeNumOne;
+    
+    // 다음 숫자의 일의자리 만들기
+    // 만든 숫자가 10보다 크면 1의 자리에 있는 숫자를 
+    // 다음에 만들어질 숫자 1의 자리로 만들기
+    if (makeNum >= 10) {
+      makeNumOne = +String(makeNum)[1];
+    } else {
+      makeNumOne = +String(makeNum)[0];
+    }
+    makeNum = +(String(one) + String(makeNumOne));
+
+    cnt++;
+  }
+  console.log(cnt);
+}
